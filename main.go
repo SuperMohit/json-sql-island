d3a7dd9e8be64ca48d500f96cf4e711997feb288
mohit <mohit.talniya@cm.com>
2021-09-10 22:38:21 +0530

    added default body

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/SuperMohit/json-sql-island/jsm"
+	"io/ioutil"
 )
 
 // This is a sample implementation of a SQL parser from a JSON file
@@ -11,7 +13,16 @@ import (
 // Second, it would traverse the syntax tree and build the expression for the SQL
 // Print the SQL to the console
 func main() {
+
+	fptr := flag.String("fpath", "input.json", "file path to read from")
+	flag.Parse()
+	data, err := ioutil.ReadFile(*fptr)
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
 	parser := jsm.QueryParser{}
-	q, _ := parser.Parse()
+	q, _ := parser.Parse(data)
 	fmt.Print(q)
 }
